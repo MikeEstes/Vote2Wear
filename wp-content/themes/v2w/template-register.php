bc9bace6dde21a7869959aec0a285c71b6d8c10b
Mike Estes <sregge@gmail.com>
2016-05-21 17:22:28 -0400

    removed AJAX import

--- wp-content/themes/v2w/template-register.php
+++ wp-content/themes/v2w/template-register.php
@@ -67,7 +67,6 @@
 	</div>
 </main>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
 <script type="text/javascript">
 jQuery(document).ready(function($) {
 
